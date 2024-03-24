6832f605daead7d551631134959a14c9e7796bfd
Chandan singh <76685431+chandan873@users.noreply.github.com>
2024-03-24 09:14:29 +0530

    addes services popup pages

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
 import React from "react";
-import { About, Footer, Header, Skills, Testimonial, Work,Whyus,Mainfooter } from "./container";
+import {
+  About,
+  Footer,
+  Header,
+  Skills,
+  Testimonial,
+  Work,
+  Whyus,
+  Mainfooter,
+} from "./container";
 import { Navbar } from "./components";
-import { Route,Routes } from 'react-router-dom';
-import Blog from './container/Blog/Blog'
+import { Route, Routes } from "react-router-dom";
+import Blog from "./container/Blog/Blog";
 import { useLocation } from "react-router-dom";
 
 import "./App.css";
 const App = () => {
   const currentLocation = useLocation();
 
-  return (  
-    <> <div className="app">
-      <Navbar />
-      {currentLocation.pathname !== "/blog" && (
-  <>
-      <Header/>
-      <Whyus/>
-      <About />
-      <Work />
-      <Footer />
-      <Mainfooter/>
-      </>
-      )}
-            {currentLocation  .pathname === "/blog" && <Blog />}
-
-
-    </div>
-
+  return (
+    <>
+      
+      <div className="app">
+        <Navbar />
+        {currentLocation.pathname !== "/blog" && (
+          <>
+            <Header />
+            <Whyus />
+            <About />
+            <Work />
+            <Footer />
+            <Mainfooter />
+          </>
+        )}
+        {currentLocation.pathname === "/blog" && <Blog />}
+      </div>
     </>
-   
-    
   );
 };
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
 import {Link} from 'react-router-dom';
-
+import { useLocation } from "react-router-dom";
 import './Navbar.css';
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const currentLocation = useLocation();
 
   return (
     <nav className="app__navbar">
@@ -14,7 +15,8 @@ const Navbar = () => {
         {/* <img src={images.logos} alt="logo" /> */}
     <a href="/"><h3 className='cursor-pointer'>INVENTIVE NEXUS</h3></a>    
       </div>
-      <ul className="app__navbar-links">
+      { currentLocation.pathname !== "/blog" && (
+        <ul className="app__navbar-links">
         {['home', 'why us','services ', 'portfolio', 'contact us'].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
             
@@ -22,8 +24,13 @@ const Navbar = () => {
           </li>
         ))}
     
-    <li><a href="/blog">Blog</a></li> 
+    <li className="app__flex p-text"><a href="/blog">Blog</a></li> 
       </ul>
+      ) }
+      
+      {currentLocation.pathname ==  "/blog" &&(
+        <a href='/'>Home</a>
+      )}
 
       <div className="app__navbar-menu">
         <HiMenuAlt4 onClick={() => setToggle(true)} />
