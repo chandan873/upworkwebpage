af6820e87a35a318e89e138d168f215895381d59
Chandan singh <76685431+chandan873@users.noreply.github.com>
2024-04-09 15:21:34 +0530

    header button text-color chaged

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -31,7 +31,7 @@ const Header = () => (
       Your premier destination for cutting-edge software solutions! At Inventive Nexus, we specialize in turning your digital dreams into reality. Whether you're envisioning a stunning website or a seamless mobile app, our team of skilled professionals is dedicated to bringing your ideas to life.
       </p>
       <div className="header_button">
-    <a href="#about"> <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Learn More</button></a> 
+    <a href="#about"> <button type="button" class="text-main  focus:outline-none  border-main font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Learn More</button></a> 
     
       </div>
     </div>
